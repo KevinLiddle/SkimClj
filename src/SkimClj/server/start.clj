5f9ee23f8f87e96fbff39326f42e365ad5cc35f1
Kevin Liddle <kliddle3@yahoo.com>
2011-09-26 19:48:43 -0500

    add file logging

--- src/SkimClj/server/start.clj
+++ src/SkimClj/server/start.clj
@@ -1,6 +1,7 @@
 (ns SkimClj.server.start
   (:import (HTTPServer Server)
-           (HTTPServer SocketCommunication)))
+           (HTTPServer SocketCommunication)
+           (HTTPServer.Logger FileLogger)))
 
 (defn -main []
   (. (new Server 3000 (new SocketCommunication)) (start)))
--- src/SkimClj/views/answer.clj
+++ src/SkimClj/views/answer.clj
@@ -2,16 +2,17 @@
   (:use [hiccup.core]
         [SkimClj.Models.Question]))
 
-(defn reaction [image-source image-alt audio-source]
+(defn reaction [image-source image-alt audio-source message]
   (html
     [:img {:src image-source :alt image-alt}]
+    [:h2 message]
     [:embed {:src audio-source :autostart "true" :hidden "true" :loop "false"}]))
 
 (defn yes []
-  (reaction "http://dl.dropbox.com/u/16607608/Skim_yes.png" "Yessss" "http://dl.dropbox.com/u/16607608/SkimYes.mp3"))
+  (reaction "http://dl.dropbox.com/u/16607608/Skim_yes.png" "Yessss" "http://dl.dropbox.com/u/16607608/SkimYes.mp3" "Skim is satisfied."))
 
 (defn sigh []
-  (reaction "http://dl.dropbox.com/u/16607608/Skim_sigh.png" "Ugh..." "http://dl.dropbox.com/u/16607608/Skim_sigh.mp3"))
+  (reaction "http://dl.dropbox.com/u/16607608/Skim_sigh.png" "Ugh..." "http://dl.dropbox.com/u/16607608/Skim_sigh.mp3" "Skim is disappointed."))
 
 (defn next-question [next-question-id]
   (html
